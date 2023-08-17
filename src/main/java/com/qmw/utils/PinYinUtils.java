c5235c7c886e78d1d4ca01b8ecebd00bed39706e
qmw <123349527@qq.com>
2023-08-17 16:05:07 +0800

    init

---
+++ src/main/java/com/qmw/utils/PinYinUtils.java
@@ -0,0 +1,70 @@
+package com.qmw.utils;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PinYinUtils {
+
+    // 格式化方式
+    private final static HanyuPinyinOutputFormat FORMAT = new HanyuPinyinOutputFormat();
+
+    static {
+        FORMAT.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        FORMAT.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        FORMAT.setVCharType(HanyuPinyinVCharType.WITH_V);
+    }
+
+    // 生僻字库
+    private static final Map<String, String> RARE_WORDS = new HashMap<>();
+
+    static {
+        RARE_WORDS.put("x", "匂");
+        RARE_WORDS.put("h", "丆");
+        RARE_WORDS.put("y", "昳");
+    }
+
+    /**
+     * 获取字符串中所有中文的拼音首字母，非中文字符直接拼接
+     *
+     * @param string string
+     * @return 拼音首字母
+     */
+    public static String getInitials(String string) {
+        if (string == null)
+            return "";
+        StringBuilder builder = new StringBuilder();
+        loop:
+        for (char c : string.toCharArray()) {
+            // 优先从生僻字中找
+            for (Map.Entry<String, String> e : RARE_WORDS.entrySet()) {
+                if (e.getValue().contains(Character.toString(c))) {
+                    builder.append(e.getKey());
+                    continue loop; // 找到对应生僻字则直接拼接，无需继续解析
+                }
+            }
+            // 生僻字没找到则解析拼音
+            if (Character.toString(c).matches("[一-龥]")) {
+                try {
+                    String[] arr = PinyinHelper.toHanyuPinyinStringArray(c, FORMAT);
+                    if (arr.length > 0 && !StringUtils.isEmpty(arr[0]))
+                        builder.append(arr[0].charAt(0));
+                    else
+                        builder.append('?');
+                } catch (BadHanyuPinyinOutputFormatCombination e) {
+                    builder.append('?');
+                }
+            } else {
+                builder.append(c);
+            }
+        }
+        return builder.toString();
+    }
+
+}
--- src/main/java/com/qmw/utils/StringUtils.java
+++ src/main/java/com/qmw/utils/StringUtils.java
@@ -2,6 +2,8 @@ package com.qmw.utils;
 
 public class StringUtils {
 
+    public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+
     public static boolean isEmpty(Object o) {
         return o == null || o.toString().trim().isEmpty();
     }
@@ -12,6 +14,38 @@ public class StringUtils {
 
     public static String trim(Object o) {
         return ifEmpty(o, "").trim();
+    }
+
+    // 驼峰转下划线
+    public static String camel2under(String s) {
+        if (isEmpty(s))
+            return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (i != 0 && Character.isUpperCase(c)) // 首字母不加下划线
+                sb.append("_");
+            sb.append(Character.toLowerCase(c));
+        }
+        return sb.toString();
+    }
+
+    // 下划线转驼峰
+    public static String under2camel(String s) {
+        if (isEmpty(s))
+            return "";
+        String[] arr = s.split("_");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            String c = arr[i];
+            if (!isEmpty(c)) {
+                if (i == 0) // 首字母不转大写
+                    sb.append(c);
+                else
+                    sb.append(Character.toUpperCase(c.charAt(0))).append(c.substring(1));
+            }
+        }
+        return sb.toString();
     }
 
 }
